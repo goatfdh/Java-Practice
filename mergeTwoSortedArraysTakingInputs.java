f88a523c091bf22eb710044ae7b73b4479875883
Soumya Priyadarshini <158063208+goatfdh@users.noreply.github.com>
2025-08-16 11:48:18 +0530

    Add files via upload
    Day 1 of Being Consistent....!!

---
+++ mergeTwoSortedArraysTakingInputs.java
@@ -0,0 +1,50 @@
+package DSA;
+
+import java.util.Scanner;
+public class mergeTwoSortedArraysTakingInputs {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter m: ");
+        int m = sc.nextInt();
+
+        int[] a = new int[m];
+        for (int i = 0; i < m; i++) {
+            a[i] = sc.nextInt();
+        }
+
+        System.out.println("Enter n:");
+        int n = sc.nextInt();
+        int[] b = new int[n];
+        for (int j = 0; j < n; j++) {
+            b[j] = sc.nextInt();
+        }
+
+        int[] c = new int[m + n];
+
+        int i = 0;
+        int j = 0;
+        int k = 0;
+
+        while (i < m && j < n) {
+            if (a[i] < b[j]) {
+                c[k++] = a[i++];
+            } else {
+                c[k++] = b[j++];
+            }
+        }
+
+        while (j < n) {
+            c[k++] = b[j++];
+        }
+        while (i < m) {
+            c[k++] = a[i++];
+        }
+
+        for (int ele : c) {
+            System.out.print(ele + " ");
+        }
+
+
+    }
+}
+
---
+++ sortZeroesOnesAndTwos.java
@@ -0,0 +1,90 @@
+package DSA;
+import java.util.Scanner;
+
+public class sortZeroesOnesAndTwos {
+//    public static void main(String[] args) {
+//        int[] arr = {0, 1, 2, 1,0,2,0,1,2,0,1};
+//        int n = arr.length;
+
+//        2 pass solution
+//        int noOfZeroes = 0;
+//        int noOfOnes = 0;
+//        int noOfTwos = 0;
+//
+//        for (int i = 0; i < n; i++) {
+//            if (arr[i] == 0) noOfZeroes++;
+//            if (arr[i] == 1) noOfOnes++;
+//        }
+//
+//        for (int i = 0; i < n; i++) {
+//            if (i < noOfZeroes) {
+//                arr[i] = 0;
+//            } else if (i< noOfZeroes +noOfOnes) {
+//                arr[i] = 1;
+//            }
+//            else{
+//                arr[i] = 2;
+//            }
+//        }
+//  3 pointer approach / Dutch Flag Algorithm / One Pass Algorithm
+
+//        int low = 0;
+//        int mid = 0;
+//        int high = n-1;
+//
+//        while(mid<=high){
+//                if(arr[mid] == 0){
+//                    int temp = arr[mid];
+//                    arr[mid] = arr[low];
+//                    arr[low] = temp;
+//                    low++;
+//                    mid++;
+//                }
+//
+//                else if(arr[mid]==1){
+//                    mid++;
+//                }
+//
+//                else if(arr[mid]==2){
+//                    int temp = arr[mid];
+//                    arr[mid] = arr[high];
+//                    arr[high] = temp;
+//                    high--;
+//                }
+//            }
+//        for(int ele:arr){
+//            System.out.print(ele + " ");
+//        }
+
+//    reading inputs from a single line / sorting 0's and 1's
+//        public static void main(String[] args){
+//            Scanner sc = new Scanner(System.in).useDelimiter("[,\\s+]");
+//            int n = sc.nextInt();
+//            int[] arr = new int[n+1];
+//
+//            for(int i=0; i<=n ;i++){
+//                arr[i] = sc.nextInt();
+//            }
+//
+//            int i=0;
+//            int j=n;
+//
+//            while(i<=j){
+//                if(arr[i]==0){
+//                    i++;
+//                } else if(arr[i]==1){
+//                    int temp = arr[i];
+//                    arr[i] = arr[j];
+//                    arr[j] = temp;
+//                    j--;
+//                }
+//            }
+//
+//            for(int ele:arr){
+//                System.out.print(ele+ " ");
+//            }
+//        }
+//
+//    }
+}
+
