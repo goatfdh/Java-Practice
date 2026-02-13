71b9b3ec1827e59fe0eee5e1616423f81f69e160
Soumya Priyadarshini <158063208+goatfdh@users.noreply.github.com>
2026-02-13 15:41:37 +0530

    Add files via upload

---
+++ Recursion/Factorial.java
@@ -0,0 +1,17 @@
+package Recursion;
+import java.util.*;
+
+public class Factorial {
+    public static int fact(int n) {
+        if (n==1) return 1;
+        int ans = n * fact(n-1);
+        return ans;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter n: ");
+        int n = sc.nextInt();
+        System.out.println(fact(n));
+    }
+
+}
